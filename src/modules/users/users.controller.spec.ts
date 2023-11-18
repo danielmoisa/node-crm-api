c388e93001fe43fe264ef982a1d1f2a8b2cbc402
Daniel Moisa <danimoisa93@gmail.com>
2023-11-18 14:46:46 +0200

    add tests for users

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,20 +1,76 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { JwtAuthGuard } from '../auth/auth.guard';
+import { PrismaService } from '../../providers/prisma/prisma.service';
+import { Request } from 'express';
+import { User } from '@prisma/client';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
+export const mockUsers: Omit<User, 'password'> = {
+  id: 1,
+  email: 'johndoe@gmail.com',
+  name: 'John Doe',
+  countryCode: 'us',
+  prefersLanguage: 'en-us',
+  profilePictureUrl: 'https://unavatar.now.sh/fallback.png',
+  timezone: 'America/Los_Angeles',
+  enabled: true,
+  prefersColorScheme: 'NO_PREFERENCE',
+  notificationEmail: 'ACCOUNT',
+  gender: 'UNKNOWN',
+  role: 'USER',
+  createdAt: new Date(),
+  updatedAt: new Date('2023-11-14T02:19:06.589Z'),
+};
+
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
-    }).compile();
+      providers: [UsersService, PrismaService],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({ canActivate: () => true }) // Override JwtAuthGuard for testing
+      .compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getAllUsers', () => {
+    it('should return an array of users', async () => {
+      // Arrange
+      const expectedUsers = mockUsers;
+      jest.spyOn(usersService, 'getAllUsers').mockResolvedValue(expectedUsers);
+
+      // Act
+      const result = await usersController.getAllUsers();
+
+      // Assert
+      expect(result).toEqual(expectedUsers);
+    });
+  });
+
+  describe('getCurrentUser', () => {
+    it('should return the current user profile', async () => {
+      // Arrange
+      const mockRequest: Partial<Request> = {
+        user: mockUsers,
+      };
+      jest.spyOn(usersService, 'getCurrentUser').mockResolvedValue(mockUsers);
+
+      // Act
+      const result = await usersController.getCurrentUser(
+        mockRequest as Request,
+      );
+
+      // Assert
+      expect(result).toEqual(mockUsers);
+    });
   });
 });
--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,18 +1,165 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from '../../providers/prisma/prisma.service';
+import { Request } from 'express';
+import { User } from '@prisma/client';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, PrismaService],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('users service should be defined', () => {
+    expect(usersService).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('should create a new user', async () => {
+      // Arrange
+      const mockUser: User = {
+        id: 1,
+        email: 'johndoe@gmail.com',
+        name: 'John Doe',
+        countryCode: 'us',
+        prefersLanguage: 'en-us',
+        profilePictureUrl: 'https://unavatar.now.sh/fallback.png',
+        timezone: 'America/Los_Angeles',
+        enabled: true,
+        prefersColorScheme: 'NO_PREFERENCE',
+        notificationEmail: 'ACCOUNT',
+        gender: 'UNKNOWN',
+        role: 'USER',
+        password: 'johndoe',
+        createdAt: new Date(),
+        updatedAt: new Date('2023-11-14T02:19:06.589Z'),
+      };
+
+      const prismaCreateUserMock = jest
+        .spyOn(prismaService.user, 'create')
+        .mockResolvedValueOnce(mockUser);
+
+      // Act
+      const createUserDto: CreateUserDto = {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: 'password',
+      };
+
+      const result = await usersService.createUser(createUserDto);
+
+      // Assert
+      expect(result).toEqual(mockUser);
+      expect(prismaCreateUserMock).toHaveBeenCalledWith({
+        data: createUserDto,
+      });
+      expect(prismaCreateUserMock).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getAllUsers', () => {
+    it('should get all users', async () => {
+      // Arrange
+      const mockUsers: Omit<User, 'password'>[] = [
+        {
+          id: 1,
+          email: 'johndoe@gmail.com',
+          name: 'John Doe',
+          countryCode: 'us',
+          prefersLanguage: 'en-us',
+          profilePictureUrl: 'https://unavatar.now.sh/fallback.png',
+          timezone: 'America/Los_Angeles',
+          enabled: true,
+          prefersColorScheme: 'NO_PREFERENCE',
+          notificationEmail: 'ACCOUNT',
+          gender: 'UNKNOWN',
+          role: 'USER',
+          createdAt: new Date(),
+          updatedAt: new Date('2023-11-14T02:19:06.589Z'),
+        },
+        {
+          id: 2,
+          email: 'johndoe2@gmail.com',
+          name: 'John Doe2',
+          countryCode: 'us',
+          prefersLanguage: 'en-us',
+          profilePictureUrl: 'https://unavatar.now.sh/fallback.png',
+          timezone: 'America/Los_Angeles',
+          enabled: true,
+          prefersColorScheme: 'NO_PREFERENCE',
+          notificationEmail: 'ACCOUNT',
+          gender: 'UNKNOWN',
+          role: 'USER',
+          createdAt: new Date(),
+          updatedAt: new Date('2023-11-14T02:19:06.589Z'),
+        },
+      ];
+
+      const prismaFindManyMock = jest
+        .spyOn(prismaService.user, 'findMany')
+        .mockResolvedValueOnce(mockUsers as User[]);
+
+      // Act
+      const result = await usersService.getAllUsers();
+
+      // Assert
+      const expectedUsers = await Promise.all(
+        mockUsers.map((user) => prismaService.expose(user)),
+      );
+      expect(result).toEqual(expectedUsers);
+      expect(prismaFindManyMock).toHaveBeenCalledWith();
+      expect(prismaFindManyMock).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('getCurrentUser', () => {
+    it('should return the current user from the request', async () => {
+      // Arrange
+      const mockUser = {
+        id: 1,
+        email: 'johndoe@gmail.com',
+        name: 'John Doe',
+        countryCode: 'us',
+        prefersLanguage: 'en-us',
+        profilePictureUrl: 'https://unavatar.now.sh/fallback.png',
+        timezone: 'America/Los_Angeles',
+        enabled: true,
+        prefersColorScheme: 'NO_PREFERENCE',
+        notificationEmail: 'ACCOUNT',
+        gender: 'UNKNOWN',
+        role: 'USER',
+        createdAt: new Date(),
+        updatedAt: new Date('2023-11-14T02:19:06.589Z'),
+      };
+      const mockRequest = { user: mockUser } as any;
+
+      // Act
+      const result = await usersService.getCurrentUser(mockRequest);
+
+      // Assert
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should return null or undefined if no user is present in the request', async () => {
+      // Arrange
+      const mockRequestWithUndefinedUser = {} as Request;
+
+      // Act
+      const resultWithUndefinedUser = await usersService.getCurrentUser(
+        mockRequestWithUndefinedUser,
+      );
+
+      // Assert
+      expect(resultWithUndefinedUser).toBeUndefined();
+    });
   });
 });
